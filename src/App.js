af3e34bdc15337b603cc45c1d34d656212f96293
Lisandro Molina <lisandromolina@Lisandros-MacBook-Pro.local>
2020-06-22 23:57:42 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import About from './About';
+import Services from './Services';
+import Products from './Products';
+import Contact from './Contact';
+import {BrowserRouter as Router, Link, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <h1>Hello</h1>
+        <Route path="/" exact component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/services" component={Services} />
+        <Route path="/products" component={Products} />
+        <Route path="/contact" component={Contact} />
+      </div>
+    </Router>
   );
 }
 
+const Home = () => (
+  <div>
+    <h1>Home</h1>
+  </div>
+)
 export default App;
---
+++ src/Services.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Services() {
+  return (
+      <div>
+        <h1>Hello Services page</h1>
+      </div>
+  );
+}
+
+export default Services;
